f7a7b140f61ef4ce4c38b7c08589c9e703f71e5e
Coihue555 <kevlar555@gmail.com>
2022-03-15 18:54:02 -0300

    benne

--- productos_app/lib/screens/product_screen.dart
+++ productos_app/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -14,11 +15,99 @@ class ProductScreen extends StatelessWidget {
             Stack(
               children: [
                 ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(Icons.arrow_back_ios_new, size: 50, color: Colors.white38),
+                  ),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 45,
+                  child: IconButton(
+                    onPressed: () {
+                        //TODO
+                    },
+                    icon: const Icon(Icons.camera_alt_outlined, size: 50, color: Colors.white38),
+                  ),
+                )
               ],
+            ),
+            _ProductForm(),
+            const SizedBox(
+              height: 100,
             )
           ],
         ),
-      )
+      ),
+      floatingActionButton: const FloatingActionButton(
+        child: Icon(Icons.save_outlined),
+        onPressed: ( value) {
+          //TODO
+        },
+      ),
+    );
+  }
+}
+
+class _ProductForm extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del Producto',
+                  labelText: 'Nombre: '
+                ),
+              ),
+              const SizedBox(height: 30,),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150',
+                  labelText: 'Precio: '
+                ),
+              ),
+              const SizedBox(height: 30,),
+              SwitchListTile.adaptive(
+                value: true,
+                title: const Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value){
+
+                }
+              ),
+              const SizedBox(height: 30,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25)),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.05),
+          blurRadius: 5,
+          offset: const Offset(0,5)
+        )
+      ]
     );
   }
 }
--- productos_app/lib/widgets/product_image.dart
+++ productos_app/lib/widgets/product_image.dart
@@ -5,15 +5,32 @@ class ProductImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 35),
       child: Container(
         decoration: _buildBoxDecoration(),
+        width: double.infinity,
+        height: 450,
+        child: const ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+          child: FadeInImage(
+            image: NetworkImage('https://via.placeholder.com/400x300/green'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fit: BoxFit.cover,
+          ),
+        )
 
       ),
     );
   }
 
   BoxDecoration _buildBoxDecoration() => BoxDecoration(
-    borderRadius: BorderRadius.only()
+    borderRadius: const BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.05),
+        blurRadius: 10,
+        offset: const Offset(0,5)
+      )
+    ]
   );
 }
